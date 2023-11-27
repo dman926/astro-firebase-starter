9b1db04297fa425f9a0eec8a4ded795adca3d5c7
Dennis James Stelmach <dj@dstelmach.com>
2023-11-27 11:57:42 -0500

    Add NOT NULLs and add quotes around SQL words.

--- supabase/migrations/20231127164434_create_tables.sql
+++ supabase/migrations/20231127164434_create_tables.sql
@@ -1,15 +1,15 @@
 CREATE TABLE clients (
   id UUID DEFAULT uuid_generate_v4(),
-  name VARCHAR(255),
+  "name" VARCHAR(255) NOT NULL,
   contact_info VARCHAR(255),
   CONSTRAINT clients_pk PRIMARY KEY (id)
 );
 
 CREATE TABLE sessions (
   id UUID DEFAULT uuid_generate_v4(),
-  client_id UUID,
-  session_date DATE,
-  location VARCHAR(255),
+  client_id UUID NOT NULL,
+  session_date DATE NOT NULL,
+  "location" VARCHAR(255),
   session_type VARCHAR(255),
   duration_hours INT,
   package VARCHAR(255),
