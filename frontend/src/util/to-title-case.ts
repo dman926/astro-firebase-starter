59a29c402890932d4d6b254ee26c88d91eacd73f
ZaxAtax <zaxatax2@gmail.com>
2023-11-27 15:29:37 -0500

    Revert "Add in grid component, resolve large majority of errors"
    This reverts commit 1d5819355b67d94fb27c81831f5a69721a9ac31b.

--- frontend/src/util/to-title-case.ts
+++
@@ -1,8 +0,0 @@
-export function toTitleCase(str: string) {
-  return str.replace(
-    /\w\S*/g,
-    (txt) => txt.charAt(0).toUpperCase() + txt.substring(1).toLowerCase(),
-  );
-}
-
-export default toTitleCase;
